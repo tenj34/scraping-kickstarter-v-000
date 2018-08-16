26c7e8c90c95617045a75b54bbaa7008d4a92390
Juan Tenezaca <tenj34@gmail.com>
2018-08-16 19:53:57 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,8 +2,16 @@
 require 'nokogiri'
 
 def create_project_hash
-  # write your code here
+  # write your code
+  # This code here will just help us open the file.
   html = File.read('fixtures/kickstarter.html')
- 
-kickstarter = Nokogiri::HTML(html)
+  kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
+
+  #iterate through all the projects
+
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project]  = {}
+  end
 end
