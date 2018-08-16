6493668109d47477279ce2f6dfa0d24ef0f3626e
Juan Tenezaca <tenj34@gmail.com>
2018-08-16 19:54:38 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,7 +12,7 @@ def create_project_hash
   #iterate through all the projects
 
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project]  = {}
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
   end
-  projects
 end
